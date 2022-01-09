d101b1c495ca54783a43d74ba75adce67de3f657
mebin-rahman <mebinfateha@gmail.com>
2022-01-09 14:22:56 +0600

    Second Commit by X

--- MyFancyJavaProject/src/Test.java
+++ MyFancyJavaProject/src/Test.java
@@ -1,14 +1,16 @@
 public class Test {
     int year;
+    String name;
 
-    Test(int year){
+    Test(int year, String name){
         this.year = year;
+        this.name = name;
     
     }
     
     public static void main(String[] args) throws Exception {
-        Test t = new Test(2022);
+        Test t = new Test(2022, "X");
         
-        System.out.println("Happy New Year "+ t.year + "!");
+        System.out.println("Happy New Year "+ t.year + ", " + t.name + "!");
     }
 }
