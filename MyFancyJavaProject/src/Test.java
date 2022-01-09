0beffc6e05d257fd61e9c0d779bece3ac2c085ed
abir171915 <mhossain171125@bscse,uiu.ac.bd>
2022-01-09 14:09:51 +0600

    singleton added by Y

--- MyFancyJavaProject/src/Test.java
+++ MyFancyJavaProject/src/Test.java
@@ -1,14 +1,23 @@
 public class Test {
+
+    private static Test t = null;
     int year;
 
-    Test(int year){
+    private Test(int year) {
         this.year = year;
-    
+
     }
-    
+
+    public static Test get_t(int year) {
+        if (t == null) {
+            t = new Test(year);
+        }
+        return t;
+    }
+
     public static void main(String[] args) throws Exception {
         Test t = new Test(2022);
-        
-        System.out.println("Happy New Year "+ t.year + "!");
+
+        System.out.println("Happy New Year " + t.year + "!");
     }
 }
